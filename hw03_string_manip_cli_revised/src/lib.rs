c62071e71c0ba44eca1be9931d1abcf6493073e4
Harry Rybacki <hrybacki@gmail.com>
2024-05-09 11:39:56 +0200

    Should have copied this over initially to make history cleaner, whoops.

---
+++ hw03_string_manip_cli_revised/src/lib.rs
@@ -0,0 +1,154 @@
+use comfy_table::{self, modifiers::UTF8_ROUND_CORNERS, presets::UTF8_FULL};
+use csv::ReaderBuilder;
+use slug::slugify;
+use std::{
+    error::Error,
+    io::{self, Read},
+};
+
+pub fn run(transmutation: &str) -> Result<String, Box<dyn Error>> {
+    // Validate the chosen transmutation or hand Err up the call stack
+    let valid_transmutation = validate_transmutation(transmutation)?;
+
+    // Collect target string from user input
+    println!("Please enter string to: '{}'", &valid_transmutation);
+    let mut target_str = String::new();
+
+    // Handle CSV case requiring multi-line input
+    match transmutation {
+        "csv" => io::stdin().read_to_string(&mut target_str)?,
+        _ => io::stdin().read_line(&mut target_str)?, // valid_transmutation() guarantees no bad inputs
+    };
+
+    // Transmute target string
+    let result = match valid_transmutation.as_ref() {
+        "lowercase" => lowercase_str(&target_str),
+        "uppercase" => uppercase_str(&target_str),
+        "no-spaces" => no_spaces_str(&target_str),
+        "trim" => trim_str(&target_str),
+        "double" => double_str(&target_str),
+        "slugify" => slugify_str(&target_str),
+        "csv" => csv_str(&target_str), // TODO clean up from testing w/ data
+        _ => unreachable!(),           // valid_transmutation guarantees this arm is unreachable
+    };
+
+    // Return transmuted string or hand Err up the cal stack
+    match result {
+        Ok(output) => Ok(output),
+        Err(e) => Err(e),
+    }
+}
+
+fn validate_transmutation(transmutation: &str) -> Result<String, Box<dyn Error>> {
+    // Validate transmutation type or hand Err up the call stack
+    let transmutations = vec![
+        "lowercase",
+        "uppercase",
+        "no-spaces",
+        "trim",
+        "double",
+        "slugify",
+        "csv",
+    ];
+
+    if transmutations.contains(&transmutation) {
+        Ok(transmutation.to_string())
+    } else {
+        return Err(From::from("received invalid transmutation type provided"));
+    }
+}
+
+fn lowercase_str(target_str: &str) -> Result<String, Box<dyn Error>> {
+    if target_str.is_empty() || target_str == "\n" {
+        Err(From::from("input string is empty"))
+    } else {
+        let output = target_str.to_lowercase();
+        Ok(output)
+    }
+}
+
+fn uppercase_str(target_str: &str) -> Result<String, Box<dyn Error>> {
+    if target_str.is_empty() || target_str == "\n" {
+        Err(From::from("input string is empty"))
+    } else {
+        let output = target_str.to_uppercase();
+        Ok(output)
+    }
+}
+
+fn no_spaces_str(target_str: &str) -> Result<String, Box<dyn Error>> {
+    if target_str.is_empty() || target_str == "\n" {
+        Err(From::from("input string is empty"))
+    } else {
+        let output = target_str.trim().replace(" ", "");
+        Ok(output)
+    }
+}
+
+fn trim_str(target_str: &str) -> Result<String, Box<dyn Error>> {
+    if target_str.is_empty() || target_str == "\n" {
+        Err(From::from("input string is empty"))
+    } else {
+        let output = target_str.trim().to_string();
+        Ok(output)
+    }
+}
+fn double_str(target_str: &str) -> Result<String, Box<dyn Error>> {
+    if target_str.is_empty() || target_str == "\n" {
+        Err(From::from("input string is empty"))
+    } else {
+        let mut output = String::new();
+        output.push_str(target_str);
+        output.push_str(target_str);
+        Ok(output)
+    }
+}
+
+fn slugify_str(target_str: &str) -> Result<String, Box<dyn Error>> {
+    if target_str.is_empty() || target_str == "\n" {
+        Err(From::from("input string is empty"))
+    } else {
+        let output = slugify(target_str);
+        Ok(output)
+    }
+}
+
+fn csv_str(target_str: &str) -> Result<String, Box<dyn Error>> {
+    if target_str.is_empty() || target_str == "\n" {
+        Err(From::from("input string is empty"))
+    } else {
+        // Create a Table to store our data
+        let mut table = comfy_table::Table::new();
+        table
+            .load_preset(UTF8_FULL)
+            .apply_modifier(UTF8_ROUND_CORNERS);
+
+        // Create a Reader
+        let mut rdr = ReaderBuilder::new()
+            .flexible(true)
+            .from_reader(target_str.as_bytes());
+
+        // Grab the headers
+        let headers = rdr.headers()?.clone();
+
+        // Convert headers into an interator so a new Row can be generated from it
+        let headers = comfy_table::Row::from(headers.iter());
+
+        // Set the headers of the table
+        table.set_header(headers);
+
+        // Iterate over the records to create a set of rows
+        for result in rdr.records() {
+            // Get the record out of the result or hand up the error
+            let record = result?;
+            // Convert StringRecord into a row
+            let row = comfy_table::Row::from(record.iter());
+            table.add_row(row);
+        }
+
+        // Generate String from table and return
+        // to_string() should be infallible because comfy_table::Table impls `Display`
+        let output = table.to_string();
+        Ok(output)
+    }
+}
---
+++ hw03_string_manip_cli_revised/src/main.rs
@@ -0,0 +1,20 @@
+use std::{env, process};
+
+use hw02_string_manip_cli::run;
+
+fn main() {
+    // Grab transmutation type from args or print error to stderr and exit
+    let transmutation = env::args().nth(1).unwrap_or_else(|| {
+        eprintln!("expected 1 argument, but got none");
+        process::exit(1);
+    });
+
+    // Perform transmutation and print resulting str or gracefully handle error and exit
+    match run(&transmutation) {
+        Err(e) => {
+            eprintln!("problem running application: {}", e);
+            process::exit(1);
+        }
+        Ok(transmuted_str) => println!("Transmutation result: \n{}", transmuted_str),
+    };
+}
