4d9b2e6e2123bfa8ed4a5d9dcdbe69aaf17f569b
Harry Rybacki <hrybacki@gmail.com>
2024-05-09 08:59:46 +0200

    Leveraged std::csv and comfy_table to handle stringification of CSV inputs. Added TODOs to track next steps.

--- hw02_string_manip_cli/src/lib.rs
+++ hw02_string_manip_cli/src/lib.rs
@@ -1,5 +1,7 @@
 use std::{error::Error, fmt, io};
 
+use comfy_table;
+use csv::ReaderBuilder;
 use slug::slugify;
 
 pub fn run(transmutation: &str) -> Result<(), Box<dyn Error>> {
@@ -13,6 +15,14 @@ pub fn run(transmutation: &str) -> Result<(), Box<dyn Error>> {
         .read_line(&mut target_str)
         .expect("Failed to read input");
 
+    // TODO CLEANUP THIS IS FOR TESTING CSV
+    let data = "\
+Name,Place,Id
+Mark,Zurich,1
+Ashley,Madrid,2
+John,New York,3
+";
+
     // Transmute target string
     let result = match valid_transmutation.as_ref() {
         "lowercase" => lowercase_str(&target_str),
@@ -21,8 +31,8 @@ pub fn run(transmutation: &str) -> Result<(), Box<dyn Error>> {
         "trim" => trim_str(&target_str),
         "double" => double_str(&target_str),
         "slugify" => slugify_str(&target_str),
-        "csv" => csv(&target_str),
-        _ => unreachable!(), // valid_transmutation guarantees this arm is unreachable
+        "csv" => csv_str(&data), // TODO clean up from testing w/ data
+        _ => unreachable!(),     // valid_transmutation guarantees this arm is unreachable
     };
 
     // Print results or hand error back up to main()
@@ -58,14 +68,14 @@ fn validate_transmutation(transmutation: &str) -> Result<String, Box<dyn Error>>
         "trim",
         "double",
         "slugify",
+        "csv",
     ];
 
     if transmutations.contains(&transmutation) {
         Ok(transmutation.to_string())
     } else {
-        Err(Box::new(MyError {
-            message: format!("{} not a valid transmutation. Try again.", transmutation),
-        }))
+        // PICK UP HERE -- REMOVE MYERROR AND REPLACE WITH THIS SYNTAX. ASK CHATGPT WTF FROM::FROM IS ABOUT
+        return Err(From::from("received invalid transmutation type provided"));
     }
 }
 
@@ -136,6 +146,39 @@ fn slugify_str(target_str: &str) -> Result<String, Box<dyn Error>> {
     }
 }
 
-fn csv(_target_str: &str) -> Result<String, Box<dyn Error>> {
-    todo!()
+fn csv_str(target_str: &str) -> Result<String, Box<dyn Error>> {
+    if target_str.is_empty() || target_str == "\n" {
+        Err(Box::new(MyError {
+            message: format!("Input string is empty."),
+        }))
+    } else {
+        // Create a Table to store our data
+        let mut table = comfy_table::Table::new();
+
+        // Create a Reader
+        let mut rdr = ReaderBuilder::new().from_reader(target_str.as_bytes());
+
+        // Grab the headers
+        let headers = rdr.headers()?.clone();
+
+        // Convert headers into an interator so a new Row can be generated from it
+        let headers = comfy_table::Row::from(headers.iter());
+
+        // Set the headers of the table
+        table.set_header(headers);
+
+        // Iterate over the records to create a set of rows
+        for result in rdr.records() {
+            // Get the record out of the result or hand up the error
+            let record = result?;
+            // Convert StringRecord into a row
+            let row = comfy_table::Row::from(record.iter());
+            table.add_row(row);
+        }
+
+        // Generate String from table and return
+        // TODO Is this okay or should we hand the exception up?
+        let output = table.to_string();
+        Ok(output)
+    }
 }
--- hw02_string_manip_cli/src/main.rs
+++ hw02_string_manip_cli/src/main.rs
@@ -1,14 +1,11 @@
-use std:: {
-    env,
-    process,
-};
+use std::{env, process};
 
 use hw02_string_manip_cli::run;
 
 fn main() {
     // Grab transmutation type from args or print error to stderr and exit
     let transmutation = env::args().nth(1).unwrap_or_else(|| {
-        eprintln!("Problem parsing arguments: transmutation not found.");
+        eprintln!("Expected 1 argument, but got none");
         process::exit(1);
     });
 
