d09d0e425a43256b97a01f7e127526223580fcf0
Harry Rybacki <hrybacki@gmail.com>
2024-05-23 14:17:49 +0200

    Server crate wasn't handling errors
    Updated return types of several functions to be Results to allow for
    error passing via the Try operators.
    Added Custom ServerError

--- hw05_client_server_msger/server/src/lib.rs
+++ hw05_client_server_msger/server/src/lib.rs
@@ -1,7 +1,10 @@
 use std::{
     collections::HashMap,
-    io::Read,
+    fmt,
+    io::{self, Read},
     net::{SocketAddr, TcpListener, TcpStream},
+    sync::{Arc, Mutex},
+    thread,
 };
 
 use common::{send_message, MessageType};
@@ -14,50 +17,113 @@ use common::{send_message, MessageType};
 /// has a current connection with
 ///
 /// TODO: How do we know when to halt the server?
-pub fn listen_and_accept(address: &str) {
+pub fn listen_and_accept(address: &str) -> Result<(), ServerError> {
     println!("Entering listen_and_accept()");
 
     // Establish TcpListener to capture incoming streams
-    let listener = TcpListener::bind(address).unwrap();
+    let listener = TcpListener::bind(address)?;
 
-    let mut clients: HashMap<SocketAddr, TcpStream> = HashMap::new();
+    let clients = Arc::new(Mutex::new(HashMap::<SocketAddr, TcpStream>::new()));
 
+    // Monitor stream and handle incoming connections
     for stream in listener.incoming() {
         println!("New stream from the listener");
 
-        // Store client in HashMap
-        let mut stream = stream.unwrap();
-        let stream_clone = stream.try_clone().unwrap();
-        let addr = stream.peer_addr().unwrap();
-        let addr_clone = addr.clone();
-        clients.insert(addr_clone, stream_clone);
+        let mut stream = stream?;
+        let clients = Arc::clone(&clients);
 
-        let msg = handle_client(clients.get(&addr).unwrap().try_clone().unwrap());
-
-        // send response ?
-        let response = MessageType::Text(String::from("Message received..."));
-        send_message(&mut stream, response);
-
-        println!("responded in listen_and_accept()");
-
-        // print msg
-        println!("{:?}", msg);
+        thread::spawn(move || match handle_client(stream, clients) {
+            Ok(()) => println!("client handled succesfully within thread"),
+            Err(e) => eprintln!("encountered server error within thread: {}", e),
+        });
     }
 
     println!("Exiting listen_and_accept()");
+    Ok(())
 }
 
-fn handle_client(mut stream: TcpStream) -> MessageType {
-    println!("Entering handle_client()");
+fn handle_client(
+    mut stream: TcpStream,
+    clients: Arc<Mutex<HashMap<SocketAddr, TcpStream>>>,
+) -> Result<(), ServerError> {
+    println!("New connection and call to handle_client()");
+
+    // Attempt to store the client in the clients HashMap
+    let addr = stream.peer_addr()?;
+    // TODO fix the unwrap
+    let mut clients_guard = clients.lock().unwrap();
+    clients_guard.insert(addr, stream.try_clone()?);
+
+    loop {
+        let msg = match read_message(&mut stream) {
+            Ok(msg) => msg,
+            Err(e) => {
+                eprintln!(
+                    "Server error encountered reading message from stream: {:?}",
+                    e
+                );
+                break;
+            }
+        };
+
+        println!("Received message from {}: {:?}", addr, msg);
+
+        let response = MessageType::Text(String::from("Message received..."));
+        if let Err(e) = send_message(&mut stream, response) {
+            println!("Error sending response: {:?}", e);
+            break;
+        }
+    }
+
+    {
+        let mut clients_guard = clients.lock().unwrap();
+        clients_guard.remove(&addr);
+    }
+    Ok(())
+}
+
+fn read_message(mut stream: &mut TcpStream) -> Result<MessageType, ServerError> {
+    println!("Entering server::read_messsage()");
+
     // get length of message
     let mut len_bytes = [0u8; 4];
-    stream.read_exact(&mut len_bytes).unwrap();
+    stream.read_exact(&mut len_bytes)?;
     let len = u32::from_be_bytes(len_bytes) as usize;
 
     // fetch message from buffer
     let mut buffer = vec![0u8; len];
-    stream.read_exact(&mut buffer).unwrap();
+    stream.read_exact(&mut buffer)?;
 
     // deseralize message
-    common::deseralize_msg(&buffer)
+    Ok(common::deseralize_msg(&buffer))
+}
+#[derive(Debug)]
+pub enum ServerError {
+    Io(io::Error),
+    SerdeJson(serde_json::Error),
+    Other(String),
+}
+
+impl fmt::Display for ServerError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ServerError::Io(err) => write!(f, "IO error: {}", err),
+            ServerError::SerdeJson(err) => write!(f, "Serialization error: {}", err),
+            ServerError::Other(err) => write!(f, "Other error: {}", err),
+        }
+    }
+}
+
+impl std::error::Error for ServerError {}
+
+impl From<io::Error> for ServerError {
+    fn from(err: io::Error) -> Self {
+        ServerError::Io(err)
+    }
+}
+
+impl From<serde_json::Error> for ServerError {
+    fn from(err: serde_json::Error) -> Self {
+        ServerError::SerdeJson(err)
+    }
 }
--- hw05_client_server_msger/server/src/main.rs
+++ hw05_client_server_msger/server/src/main.rs
@@ -6,10 +6,13 @@ fn main() {
     // Process parameters to determine hostname and what not for server
     let args: Vec<String> = env::args().collect();
     let address = common::get_hostname(args);
-    println!("Server address set to: {}", address);
+    println!("Launching server on address: {}", address);
 
     // Create the server and begin routing
-    let _server = server::listen_and_accept(&address);
+    match server::listen_and_accept(&address) {
+        Err(e) => eprintln!("Server error: {}", e),
+        Ok(()) => println!("Server shutting down... goodnight."),
+    }
 
-    println!("Leaving server::main()");
+    println!("leaving server::main()");
 }
