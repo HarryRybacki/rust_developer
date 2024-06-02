1b214f2674ddc50ac711dddb83b3a5dd127cdcef
Harry Rybacki <hrybacki@gmail.com>
2024-06-02 14:18:36 +0200

    Library logger leverages the global logger from Client/Server binary
    Closes #6

--- hw06_professionalize_client_server_app/common/src/main.rs
+++
@@ -1,8 +0,0 @@
-use env_logger::{Builder, Env};
-
-/// Solely used to initialize the logger
-fn main() {
-    // Establish our logger
-    let env = Env::default().filter_or("RUST_LOG", "info");
-    Builder::from_env(env).init();
-}
